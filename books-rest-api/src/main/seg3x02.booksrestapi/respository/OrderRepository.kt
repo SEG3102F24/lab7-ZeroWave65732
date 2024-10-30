19cb772dd9012f910e29e6468af56df213ce0c6e
ZeroWave65732 <williammenard@protonmail.com>
2024-10-30 15:42:27 -0400

    Server

---
+++ books-rest-api/src/main/seg3x02.booksrestapi/respository/OrderRepository.kt
@@ -0,0 +1,7 @@
+package seg3x02.booksrestapi.repository
+
+import org.springframework.data.repository.CrudRepository
+import seg3x02.booksrestapi.entities.Order
+
+interface OrderRepository : CrudRepository<Order, Long>
+
